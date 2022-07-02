3cd28d108ec3ce0a9356a3aefb3bf680eb1ff312
Nathan Ingle <elgni.nahtan@gmail.com>
2022-07-02 13:01:15 +1000

    Use anyhow; filter on min size

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::fs::{self, File};
-use std::io::{self, Read};
+use std::io::Read;
 use std::os::unix::fs::MetadataExt;
 use std::path::{Path, PathBuf};
+
+use anyhow::{bail, Context, Result};
 
 const BUFFER_LEN: usize = 1024 * 1024; // 1 MiB
 
@@ -22,7 +24,7 @@ struct DedupFile {
 }
 
 // Calculate the CRC32 checksum of a file.
-fn calc_crc32(path: &Path) -> io::Result<u32> {
+fn calc_crc32(path: &Path) -> Result<u32> {
     use crc32fast::Hasher;
 
     let mut file = File::open(path)?;
@@ -43,17 +45,17 @@ fn calc_crc32(path: &Path) -> io::Result<u32> {
 
 // Recursively descend through a filesystem hierarchy, collecting information about only regular
 // files.
-fn visit(path: &Path) -> io::Result<Vec<DedupFile>> {
+fn visit(path: &Path, min_size: u64) -> Result<Vec<DedupFile>> {
     let metadata = fs::symlink_metadata(path)?;
     let mut dedup_files: Vec<DedupFile> = Vec::new();
 
     if metadata.is_dir() {
         // Recurse over directory contents.
         for dir_entry in fs::read_dir(path)? {
-            let mut child_dedup_files: Vec<DedupFile> = visit(&dir_entry?.path())?;
+            let mut child_dedup_files: Vec<DedupFile> = visit(&dir_entry?.path(), min_size)?;
             dedup_files.append(&mut child_dedup_files);
         }
-    } else if metadata.is_file() {
+    } else if metadata.is_file() && metadata.len() >= min_size {
         // Record info about this file.
         let dedup_file = DedupFile {
             path: fs::canonicalize(path)?,
@@ -71,7 +73,33 @@ fn visit(path: &Path) -> io::Result<Vec<DedupFile>> {
     Ok(dedup_files)
 }
 
-fn main() -> io::Result<()> {
+// Parse a string describing the size of a file, with optional SI or IEC unit prefix.
+fn parse_file_size_spec(s: &str) -> Result<u64> {
+    let mut t: String = s.to_owned();
+    t.make_ascii_lowercase();
+    let (num_str, suffix) = t
+        .find(|c: char| c.is_ascii_alphabetic())
+        .map(|i| t.split_at(i))
+        .unwrap_or((&t, ""));
+    let multiplier: u64 = match suffix {
+        "" => 1,
+        "k" | "kb" => 1_000,
+        "m" | "mb" => 1_000_000,
+        "g" | "gb" => 1_000_000_000,
+        "t" | "tb" => 1_000_000_000_000,
+        "ki" | "kib" => 1_024,
+        "mi" | "mib" => 1_024 * 1_024,
+        "gi" | "gib" => 1_024 * 1_024 * 1_024,
+        "ti" | "tib" => 1_024 * 1_024 * 1_024 * 1_024,
+        _ => bail!("Failed to parse file size (bad multiplier -- got {:?})", s),
+    };
+    num_str
+        .parse()
+        .map(|num: u64| num * multiplier)
+        .with_context(|| format!("Failed to parse file size (bad number -- got {:?})", s))
+}
+
+fn main() -> Result<()> {
     use clap::{App, Arg};
 
     // Parse command-line arguments.
@@ -83,6 +111,12 @@ fn main() -> io::Result<()> {
                 .required(true)
                 .index(1),
         )
+        .arg(
+            Arg::with_name("MIN_SIZE")
+                .long("min-size")
+                .help("Ignore files smaller than this (bytes)")
+                .default_value("100000"),
+        )
         .get_matches();
 
     let target = Path::new(
@@ -91,9 +125,14 @@ fn main() -> io::Result<()> {
             .expect("Failed to read PATH from command-line arguments"),
     );
 
+    let min_size_str = matches
+        .value_of("MIN_SIZE")
+        .expect("Failed to find MIN_SIZE argument despite clap default_value");
+    let min_size: u64 = parse_file_size_spec(min_size_str)?;
+
     // Traverse the filesystem.  Since we expect to be limited by disk I/O, there may be no
     // performance benefit from parallelism.
-    let dedup_files = visit(target)?;
+    let dedup_files = visit(target, min_size)?;
 
     // For now, we output results in CSV format to placate Rust's dead code analysis.
     for d in dedup_files {
